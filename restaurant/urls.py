a22ea7af375bc05492cb518d8a3326a8492de6df
palakkhatri2098@gmail.com <palakkhatri2098@gmail.com>
2025-03-11 18:36:38 +0530

    updated booking api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin 
-from django.urls import path 
+from django.urls import path,include
 from . import views 
   
+
+
 urlpatterns = [ 
-    # path('', views.sayHello, name='sayHello'), 
-    path('', views.index, name='index')
+    path('menu-items/', views.MenuItemView.as_view(), name='menu-items'),
+    path('menu-items/<int:pk>/',views.SingleMenuItemView.as_view(), name='single-menu-items'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-def sayHello(request):
-    return HttpResponse('Hello World')
+from rest_framework import generics,viewsets,permissions
+from .serializers import MenuSerializer,BookingSerializer
+from .models import Menu,Booking
 
-def index(request):
-    return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+   queryset = Booking.objects.all()
+   serializer_class = BookingSerializer
+#    permission_classes = [permissions.IsAuthenticated] 
