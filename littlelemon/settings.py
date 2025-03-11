e8aa4e479d9fef862db2838ae057fbc53008359f
palakkhatri2098@gmail.com <palakkhatri2098@gmail.com>
2025-03-11 17:35:56 +0530

    model creation for booking and menu

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -75,10 +75,17 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES ={
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'Radixweb8',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -119,9 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # This is not needed for app-specific static files, but useful if you have a global static folder
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # Uncomment if using a global static folder
-]
+
 
 # For production, to collect all static files in one place
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
